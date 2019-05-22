662c352b1a95a1d30b00b766ee2ff51efd4787e6
Ivan Balandzin <ivan_balandzin@epam.com>
2019-05-23 00:08:00 +0300

    Update prov_tomcat.sh

--- prov_tomcat.sh
+++ prov_tomcat.sh
@@ -5,15 +5,14 @@ if [ ! -d /apps/tomcat ]; then
 fi
 cd /apps/tomcat
 
-if [ ! -d /apps/tomcat/apache-tomcat-8.5.9 ]; then
+if [ ! -d /apps/tomcat/apache-tomcat-8.5.9 ]
+then
 	tar xvf /vagrant/apache-tomcat-8.5.9.tar.gz 
-#sed -i 's/Server port=\"8005\" shutdown=\"SHUTDOWN\"/Server port=\"9'$i'10\" shutdown=\"SHUTDOWN\"/' apache-tomcat-8.5.9/conf/server.xml 
-#sed -i 's/Connector port=\"8080\" protocol=\"HTTP/Connector port=\"9'$i'11\" protocol=\"HTTP/' apache-tomcat-8.5.9/conf/server.xml 
-#sed -i 's/Connector port=\"8009\" protocol=\"AJP/Connector port=\"9'$i'12\" protocol=\"AJP/' apache-tomcat-8.5.9/conf/server.xml 
+	yum install -y java-11-openjdk.x86_64
 fi
-yum install -y java-11-openjdk.x86_64
 
-if [ ! -f /etc/systemd/system/tomcat.service ]; then
+if [ ! -f /etc/systemd/system/tomcat.service ]
+then
 	cat <<EOT >> /etc/systemd/system/tomcat.service
 [Unit]
 Description=Tomcat - instance $i
@@ -21,10 +20,6 @@ After=network.target
 
 [Service]
 Type=forking
-
-#User=tomcat
-#Group=tomcat
-
 
 Environment="JAVA_HOME=//usr/lib/jvm/java-11-openjdk-11.0.1.13-3.el7_6.x86_64/
 Environment="CATALINA_PID=/apps/tomcat/apache-tomcat-8.5.9/run/tomcat.pid"
@@ -43,5 +38,6 @@ EOT
 fi
 systemctl daemon-reload
 chmod +x /apps/tomcat/apache-tomcat-8.5.9/bin/*.sh
+cp /vagrant/clusterjsp.war /apps/tomcat/apache-tomcat-8.5.9/
 systemctl enable tomcat
 systemctl start tomcat
